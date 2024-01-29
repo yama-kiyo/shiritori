474684d99f0318987fd53fc0990208b69e18ae3c
KIYOSHI YAMAMOTO <43377014+yama-kiyo@users.noreply.github.com>
2024-01-29 13:10:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def play():
         return jsonify({"status": "error", "message": "前の単語の最後の文字と一致しません。"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
